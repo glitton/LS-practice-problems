d52a617b40bf997248a2b6d71fb96b4c7d2c01b8
Generosa Litton <glcodeworks@gmail.com>
2024-09-28 15:37:27 -0500

    redo easy1 first 3 problems

---
+++ 1_easy1/evenNumbers.js
@@ -0,0 +1,7 @@
+// for (let number = 2; number <= 99; number += 2) {
+//   console.log(number);
+// }
+
+for (let number = 1; number < 100; number++) {
+  if (number % 2 === 0) console.log(number);
+}
---
+++ 1_easy1/oddNumbers.js
@@ -0,0 +1,7 @@
+// for (let i = 1; i <= 99; i++) {
+//   if (i % 2 === 1) console.log(i);
+// }
+
+for (let number = 1; number < 100; number += 2) {
+  console.log(number);
+}
