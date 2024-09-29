9b5e7a3b9a94d98d6b89b30fbd2d4c2fa18f0945
Generosa Litton <glcodeworks@gmail.com>
2024-09-28 23:41:47 -0500

    power function

--- 2_easy2/multiply.js
+++ 2_easy2/multiply.js
@@ -2,10 +2,21 @@ function multiply(num1, num2) {
   return num1 * num2;
 }
 
-function square(num) {
-  return multiply(num, num);
-}
-console.log(multiply(5, 3) === 15); // logs true
+// function square(num) {
+//   return multiply(num, num);
+// }
 
-console.log(square(5) === 25); // logs true
-console.log(square(-8) === 64); // logs true
+function power(num, exp) {
+  let value = 1;
+  for (let rounds = 1; rounds <= exp; rounds++) {
+    value *= multiply(1, num);
+  }
+  return value;
+}
+
+// console.log(multiply(5, 3) === 15); // logs true
+
+// console.log(square(5) === 25); // logs true
+// console.log(square(-8) === 64); // logs true
+
+console.log(power(2, 4));
