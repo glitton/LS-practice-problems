c7e7c90d2b1ce1fbf6ee04f04eac504367739a5e
Generosa Litton <glcodeworks@gmail.com>
2024-10-18 22:56:36 -0500

    staggered lengths console.log

--- string-text-processing/wordLenghts.js
+++ string-text-processing/wordLenghts.js
@@ -10,13 +10,13 @@ function wordLengths(string) {
 console.log(wordLengths("cow sheep chicken"));
 // ["cow 3", "sheep 5", "chicken 7"]
 
-// wordLengths("baseball hot dogs and apple pie");
+console.log(wordLengths("baseball hot dogs and apple pie"));
 // // ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
 
-// wordLengths("It ain't easy, is it?");
+console.log(wordLengths("It ain't easy, is it?"));
 // // ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
 
-// wordLengths("Supercalifragilisticexpialidocious");
+console.log(wordLengths("Supercalifragilisticexpialidocious"));
 // // ["Supercalifragilisticexpialidocious 34"]
 
 console.log(wordLengths("")); // []
