7532f7e1eefc48e5870c4ee07e13b039c4ccb475
Generosa Litton <glcodeworks@gmail.com>
2024-10-24 17:26:36 -0500

    rotate rightmost digits

--- 01_Medium1/rotateArray.js
+++ 01_Medium1/rotateArray.js
@@ -30,15 +30,16 @@ function rotateArray(array) {
   }
 
   let arrayCopy = array.slice(1);
+  // console.log(arrayCopy);
 
   return arrayCopy.concat(array[0]);
 }
 
 // console.log(rotateArray([7, 3, 5, 2, 9, 1])); // [3, 5, 2, 9, 1, 7]
-// // rotateArray(["a", "b", "c"]); // ["b", "c", "a"]
-// // rotateArray(["a"]); // ["a"]
-// // rotateArray([1, "a", 3, "c"]); // ["a", 3, "c", 1]
-// // rotateArray([{ a: 2 }, [1, 2], 3]); // [[1, 2], 3, { a: 2 }]
+// console.log(rotateArray(["a", "b", "c"])); // ["b", "c", "a"]
+// console.log(rotateArray(["a"])); // ["a"]
+// console.log(rotateArray([1, "a", 3, "c"])); // ["a", 3, "c", 1]
+// console.log(rotateArray([{ a: 2 }, [1, 2], 3])); // [[1, 2], 3, { a: 2 }]
 // console.log(rotateArray([])); // []
 
 // // return `undefined` if the argument is not an array
@@ -46,6 +47,63 @@ function rotateArray(array) {
 // console.log(rotateArray(1)); // undefined
 
 // the input array is not mutated
-let array = [1, 2, 3, 4];
-console.log(rotateArray(array)); // [2, 3, 4, 1]
-console.log(array); // [1, 2, 3, 4]
+// let array = [1, 2, 3, 4];
+// console.log(rotateArray(array)); // [2, 3, 4, 1]
+// console.log(array); // [1, 2, 3, 4]
+
+/*
+Rotate Rightmost Digits
+Write a function that rotates the last count digits of a number. 
+To perform the rotation, move the first of the digits that you want to rotate 
+to the end and shift the remaining digits to the left.
+
+P:
+Input: two numbers, the original number, count digits to rotate
+Output: The rotated number
+Rules:
+- 
+- Rotate the last count digits of the original number
+- Leave the first digits (not part of count) in the same order
+- Apply the same rules as in rotateArray:
+  -- slice of the first of the digits you want to rotate, append it to the end of number
+  -- Join the first digits with the last count rotated digits
+
+E:
+D: Strings
+A:
+- Convert original number to a string
+  - Use String() method
+- Split the string into two parts: the first part to remain unchanged and the second part to be rotated
+  - firstPart = string.slice(0, length-count)
+  - secondPart = string.slice(length - count)
+- Rotate the second part
+  - Use algo from previous function
+- Join the first part with the rotated second part
+  - firstPart.concat(secondPart)
+- Convert the string back to a number and return it
+  - Return number using Number()
+
+*/
+
+function rotateRightmostDigits(number, count) {
+  let numberString = String(number);
+
+  let firstPart = numberString.slice(0, numberString.length - count);
+  let secondPart = numberString.slice(numberString.length - count);
+  let rotatedRightstring = firstPart.concat(rotateString(secondPart));
+
+  return Number(rotatedRightstring);
+}
+
+function rotateString(string) {
+  return string.slice(1).concat(string[0]);
+}
+
+console.log(rotateRightmostDigits(735291, 1)); // 735291
+console.log(rotateRightmostDigits(735291, 2)); // 735219
+console.log(rotateRightmostDigits(735291, 3)); // 735912
+console.log(rotateRightmostDigits(735291, 4)); // 732915
+console.log(rotateRightmostDigits(735291, 5)); // 752913
+console.log(rotateRightmostDigits(735291, 6)); // 352917
+
+// console.log(rotateString("735291"));
